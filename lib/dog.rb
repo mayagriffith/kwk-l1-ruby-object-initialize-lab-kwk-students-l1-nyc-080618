198d1a036b0b190cd429c789c0ef0f4d88a8d4b9
mayagriffith <mayaegriffith@gmail.com>
2018-08-10 14:47:13 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,21 +3,6 @@ class Dog
     @name = name
     @breed = breed
   end
-  def name
-    @name
-  end
-  
-  def name= (dogs_name)
-    @name = dogs_name
-  end
-  
-  def breed
-    @breed 
-  end
-  
-  def breed=(new_breed)
-    @breed = new_breed
-  end
 
 end
 
